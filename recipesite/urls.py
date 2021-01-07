5f6305aafc74e409bb2eb296578f65b23087c758
akshaynorth <akshaynorth@outlook.com>
2021-01-07 14:29:33 -0600

    Add the recipe app URLS to urls.py

--- recipesite/urls.py
+++ recipesite/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 urlpatterns = [
     path('', include('pages.urls')),
     path('pages/', include('pages.urls')),
+    path('recipe/', include('recipe.urls')),
     path('admin/', admin.site.urls)
 ]
