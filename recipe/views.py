f4fd15e742a62d2c39a9a8b2d1bd9a75de1e3e67
akshaynorth <akshaynorth@outlook.com>
2021-01-08 00:00:38 -0600

    Implement recipe search view Django database queries

--- recipe/views.py
+++ recipe/views.py
@@ -6,8 +6,9 @@ import json
 import logging
 import datetime
 
-from django.http import JsonResponse
+from django.http import JsonResponse, Http404
 from django.views.decorators.csrf import csrf_protect
+from django.db.models import Q
 from .models import Recipe, RecipeIngredient, RecipeInstruction
 
 logger = logging.getLogger(__name__)
@@ -68,7 +69,56 @@ def create(requests):
 
 
 def search_recipe(requests):
-    return None
+    try:
+        if requests.method == 'POST':
+            form_data = requests.POST
+
+            query_dict = dict()
+
+            recipe_type = form_data.get('type', None)
+            if recipe_type and recipe_type.upper() != 'ALL':
+                query_dict.update(
+                    dict(
+                      type=recipe_type
+                    )
+                )
+
+            ingredient_list = form_data.get('ingredient_list', None)
+            ingredient_query_args = None
+
+            if ingredient_list:
+                ingredient_search_type = form_data.get('ingredient_search_type', None)
+                if ingredient_search_type and ingredient_search_type.upper() != 'ALL':
+                    query_dict.update(
+                        dict(ingredient__in=ingredient_list)
+                    )
+                else:
+                    for ingredient in ingredient_list:
+                        if ingredient_query_args:
+                            ingredient_query_args = ingredient_query_args[0] & Q(ingredient_icontains=ingredient)
+
+                        else:
+                            ingredient_query_args = (
+                                Q(ingredient__icontains=ingredient),
+                            )
+
+                if ingredient_query_args:
+                    recipes = Recipe.objects.filter(ingredient_query_args, query_dict)
+                else:
+                    recipes = Recipe.objects.filter(query_dict)
+
+                return render(
+                    'pages/submit-recipe.html',
+                    recipes_list=recipes
+                )
+
+        else:
+            raise ValueError('Only HTTP POST supported for search recipes')
+    except Exception as e:
+        logger.exception('Could not complete search for recipe')
+        return Http404('Could not search recipe: {}'.format(str(e)))
+
+    return Http404('Unexpected error has occurred while searching for recipe')
 
 
 def download_recipe_image(requests, recipe_id):
