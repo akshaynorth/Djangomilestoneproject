efc2ae6c33cb6930fd142eb658678f1bd736f1e6
akshaynorth <akshaynorth@outlook.com>
2021-01-10 02:48:06 -0600

    Add the recipe django models for the admin app

--- recipe/admin.py
+++ recipe/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .views import Recipe, RecipeIngredient, RecipeInstruction
+
 # Register your models here.
+
+admin.register(Recipe, RecipeIngredient, RecipeInstruction)
