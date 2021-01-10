41276f69b9f0c15c15e3a64aaf6c8de0e7a61930
akshaynorth <akshaynorth@outlook.com>
2021-01-10 16:44:37 -0600

    Fix recipesite homepage url definition

--- recipesite/urls.py
+++ recipesite/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('pages.urls')),
+    path('', include('recipe.urls')),
     path('pages/', include('pages.urls')),
     path('recipe/', include('recipe.urls')),
     path('admin/', admin.site.urls)
