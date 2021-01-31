10c797eba5cb92f69fa5fe75ccaae35bc105881c
akshaynorth <akshaynorth@outlook.com>
2021-01-31 13:48:15 -0600

    Change checkout session url mapping to be right after hostname

--- recipe_pay/urls.py
+++ recipe_pay/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('success', views.payment_success, name='pay_success'),
     path('pay_cancel', views.payment_cancel, name='pay_cancel'),
     path('order_review', views.order_review, name='order_review'),
-    re_path('^create-checkout-session$', views.create_checkout_session, name='create_checkout_session'),
+    # mapped from main site URL as create checkout session
+    path('', views.create_checkout_session, name='create_checkout_session'),
 ]
--- recipesite/urls.py
+++ recipesite/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('recipe/', include('recipe.urls')),
     path('recipe_cart/', include('recipe_cart.urls')),
     path('recipe_pay/', include('recipe_pay.urls')),
-    re_path('^create-checkout-session', include('recipe_pay.urls')),
+    re_path(r'^create-checkout-session$', include('recipe_pay.urls')),
     path('admin/', admin.site.urls)
 ]
