99ba75c7ea61a735c39cb008d2a62b3344817930
akshaynorth <akshaynorth@outlook.com>
2021-01-31 23:47:48 -0600

    Uncomment getting ordered recipes in show_ordered_recipes view

--- recipe_orders/views.py
+++ recipe_orders/views.py
@@ -13,15 +13,14 @@ logger = logging.getLogger(__name__)
 
 @login_required()
 def show_ordered_recipes(request):
-    recipes = None
-    # try:
-    #     if request.method == 'GET':
-    #         recipes = OrderedRecipe.objects.filter(user=request.user)
-    #     else:
-    #         raise ValueError('Unsupported HTTP method to show ordered recipes: {}'.format(request.method))
-    # except:
-    #     logger.exception('Could not obtain ordered recipes')
-    #     raise Http404()
+    try:
+        if request.method == 'GET':
+            recipes = OrderedRecipe.objects.filter(user=request.user)
+        else:
+            raise ValueError('Unsupported HTTP method to show ordered recipes: {}'.format(request.method))
+    except:
+        logger.exception('Could not obtain ordered recipes')
+        raise Http404()
 
     return render(
         request,
